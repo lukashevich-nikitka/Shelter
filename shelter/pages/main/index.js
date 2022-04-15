2dd142895a8f3252328b114074f3e5da81cb3312
Nikita <lukashevich-nikitka@bk.ru>
2022-04-15 12:53:23 +0300

    feat: add burger menu for main and pets pages

--- shelter/pages/main/index.js
+++ shelter/pages/main/index.js
@@ -1,5 +1,8 @@
 const burger = document.querySelector('.burger');
+const burgerList = document.querySelector('.close-burger-menu');
+
 function pushMenu() {
-    burger.classList.toggle('change')
+    burger.classList.toggle('change');
+    burgerList.classList.toggle('open-burger-menu');
 }
 burger.addEventListener('click', pushMenu)

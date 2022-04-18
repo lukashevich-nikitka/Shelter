21292006d31b5c50028a68144052dfb7fff524a9
Nikita <lukashevich-nikitka@bk.ru>
2022-04-19 00:01:11 +0300

    feat: add some fix in header and footer. Submit!

--- shelter/pages/pets/index.js
+++ shelter/pages/pets/index.js
@@ -1,8 +1,10 @@
 const burger = document.querySelector('.burger');
 const burgerList = document.querySelector('.close-burger-menu');
+const areaUnderActiveMenu = document.querySelector('.area-under-menu');
 
 function pushMenu() {
     burger.classList.toggle('change');
     burgerList.classList.toggle('open-burger-menu');
+    areaUnderActiveMenu.classList.toggle('area-under-menu-active');
 }
 burger.addEventListener('click', pushMenu)

158d1b4ace92b37aaa673458f545716f0bed8455
Nikita <lukashevich-nikitka@bk.ru>
2022-04-21 16:00:04 +0300

    feat: add popup for main page. Need to fix close process and check on differnt view

--- shelter/pages/main/for2pages.js
+++ shelter/pages/main/for2pages.js
@@ -1,12 +1,14 @@
 const burger = document.querySelector('.burger');
 const burgerList = document.querySelector('.close-burger-menu');
 const areaUnderActiveMenu = document.querySelector('.area-under-menu');
+const body = document.querySelector('body');
 const closeItemsArray = [burgerList, burger, areaUnderActiveMenu];
 
 function pushMenu() {
     burger.classList.toggle('change');
     burgerList.classList.toggle('open-burger-menu');
     areaUnderActiveMenu.classList.toggle('area-under-menu-active');
+    body.classList.toggle('delete-scroll');
 }
 closeItemsArray.forEach((el) => el.addEventListener('click', pushMenu));
 
--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -5,6 +5,9 @@ const friendsNamesCollection = document.querySelectorAll('.friends-card-text');
 const rightArrow = document.querySelector('.right-arrow');
 const leftArrow = document.querySelector('.left-arrow');
 const arrows = [rightArrow, leftArrow];
+const popupCards = document.querySelectorAll('.friends-card');
+
+/*slider*/
 
 function pushSlider() {
     friendsImagesCollection.forEach((image, index) => image.src = info[index+3].img);
@@ -14,3 +17,48 @@ function pushSlider() {
 }
 
 arrows.forEach((el) => el.addEventListener('click', pushSlider));
+
+/*------------------------------------------------------------------------------------*/
+
+const closePopup = document.querySelector('.modale-window-close');
+const modaleImage = document.querySelector('.modale-image');
+const modaleTitle = document.querySelector('.modale-title');
+const modaleText = document.querySelector('.modale-text');
+const modaleParameters = document.querySelector('.modale-parameters');
+const closePopupButton = document.querySelector('.popup-out');
+const areaUnderActiveMenu = document.querySelector('.area-under-menu');
+const body = document.querySelector('body');
+const closePopupArray = [body, closePopupButton];
+
+/*popup*/
+
+function pushPopup(event) {
+    closePopup.classList.toggle('modale-window-active');
+    body.classList.toggle('delete-scroll');
+    areaUnderActiveMenu.classList.add('area-under-menu-active');
+    if (event.currentTarget.classList.contains('friends-card')) {
+        let link = event.currentTarget.firstElementChild.nextElementSibling.textContent
+                for (let i = 0; i < info.length; i++) {
+            if (info[i].name === link) {
+                modaleImage.src = info[i].img;
+                modaleTitle.firstElementChild.textContent = info[i].name;
+                modaleTitle.lastElementChild.textContent = `${info[i].type + ` - ` + info[i].breed}`;
+                modaleText.textContent = info[i].description;
+                modaleParameters.firstElementChild.textContent = `${'Age: ' + info[i].age}`;
+                modaleParameters.firstElementChild.nextElementSibling.textContent = `${'Inoculations: ' + info[i].inoculations}`;
+                modaleParameters.lastElementChild.previousElementSibling.textContent = `${'Diseases: ' + info[i].diseases}`;
+                modaleParameters.lastElementChild.textContent = `${'Parasites: ' + info[i].parasites}`;
+            }
+        }
+    }
+};
+
+function closeModaleWindow() {
+    closePopup.classList.toggle('modale-window-active');
+    areaUnderActiveMenu.classList.remove('area-under-menu-active');
+}
+
+closePopupButton.addEventListener('click', closeModaleWindow);
+popupCards.forEach((el) => el.addEventListener('click', pushPopup))
+
+
